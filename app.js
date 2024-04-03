7e388b21fadfd947f3f26315b33ed79613fe054a
Vladislav Nevzorov <hoverrdev@gmail.com>
2024-04-03 07:52:09 +0300

    Итерация по Map

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 'use strict';
 
 const weatherMap = new Map([
-	['London', '10'],
-	['Moscow', '7'],
+	['London', 10],
+	['Moscow', 7],
+	['Paris', 14],
 ]);
-console.log(weatherMap);
 
-const weatherObject = {
-	london: 10,
-	moscow: 7,
-	paris: 14,
-};
+for (const [key, value] of weatherMap) {
+	console.log(key);
+	console.log(value);
+}
 
-console.log(Object.entries(weatherObject));
-const weatherMap2 = new Map(Object.entries(weatherObject));
-console.log(weatherMap2);
-console.log(weatherMap2.size);
+console.log([...weatherMap]);
+console.log([...weatherMap.keys()]);
+console.log([...weatherMap.values()]);
