0b8fb8609f81e8f784cff00ab4a60753c6678b68
Vladislav Nevzorov <hoverrdev@gmail.com>
2024-04-03 07:47:16 +0300

    Быстрое создание Map

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
 'use strict';
 
-const weatherMap = new Map();
-weatherMap.set('London', '10').set('Moscow', '7');
-
-console.log(weatherMap.get('Moscow'));
-console.log(weatherMap.get('not found'));
-
-console.log(weatherMap.has('Moscow'));
-console.log(weatherMap.has('not found'));
-console.log(weatherMap.delete('London'));
-weatherMap.clear();
-
-const arr = [1, 2, 3];
-weatherMap
-	.set(1, 5)
-	.set(true, 'yes')
-	.set(true, 'yes!')
-	.set(false, 'no')
-	.set(arr, 'array')
-	.set({ a: 1 }, { b: 1 });
-
+const weatherMap = new Map([
+	['London', '10'],
+	['Moscow', '7'],
+]);
 console.log(weatherMap);
-console.log(weatherMap.size);
 
-console.log(weatherMap.get(arr));
-console.log(weatherMap.get({ a: 1 }));
+const weatherObject = {
+	london: 10,
+	moscow: 7,
+	paris: 14,
+};
+
+console.log(Object.entries(weatherObject));
+const weatherMap2 = new Map(Object.entries(weatherObject));
+console.log(weatherMap2);
+console.log(weatherMap2.size);
