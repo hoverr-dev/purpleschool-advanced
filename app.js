e5dc93a74b9089092c55bbe882a4e7a2c61cad27
Vladislav Nevzorov <hoverrdev@gmail.com>
2024-04-07 12:43:19 +0300

    Создание таймеров

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
 'use strict';
 
-const date = new Date();
-console.log(date);
+console.log(1);
 
-console.log(new Intl.DateTimeFormat('ru-RU').format(date));
+const messages = ['Bash!', 'Boom!'];
 
-const options1 = {
-	hour: 'numeric',
-	minute: 'numeric',
-};
-console.log(new Intl.DateTimeFormat('ru-RU', options1).format(date));
-
-const options2 = {
-	hour: 'numeric',
-	minute: 'numeric',
-	month: 'short',
-	weekday: 'short',
-	year: '2-digit',
-};
-console.log(new Intl.DateTimeFormat('en-US', options2).format(date));
-console.log(new Intl.DateTimeFormat('ru-RU', options2).format(date));
-
-console.log(navigator.language);
-console.log(
-	new Intl.DateTimeFormat(navigator.language, options1).format(date)
+const boomTimer = setTimeout(
+	(message1, message2) => {
+		console.log(2);
+		console.log(message1);
+		console.log(message2);
+	},
+	1000,
+	...messages
 );
+// clearTimeout(boomTimer);
+
+console.log(3);
