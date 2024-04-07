27b2c5d799fb7af6fe72cbe5687387494ede069c
Vladislav Nevzorov <hoverrdev@gmail.com>
2024-04-07 13:19:52 +0300

    Упражнение - Таймер пиццы

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 'use strict';
 
-const interval = setInterval(() => {
-	console.log(new Date());
-}, 1000);
+/* 
+	Таймер пиццы
+	Сделать таймер пиццы (функция, принимающая время),
+	который будет выводить в консоль секунды,
+	оставшиесся до готовности пиццы и собощения по готовности.
+	00:04
+	00:03
+	00:02
+	00:01
+	00:00
+	!!!!
+*/
 
-const timer = setTimeout(() => {
-	clearInterval(interval);
-}, 5000);
+function pizzaTimer(ms) {
+	const end = new Date().getTime() + ms;
+	const interval = setInterval(() => {
+		console.log(
+			new Intl.DateTimeFormat('ru-RU', {
+				minute: 'numeric',
+				second: 'numeric',
+			}).format(end + 100 - new Date())
+		);
+	}, 1000);
+	setTimeout(() => {
+		clearInterval(interval);
+		console.log('!!!!');
+	}, ms);
+}
 
-console.log(interval);
-console.log(timer);
+pizzaTimer(5000);
