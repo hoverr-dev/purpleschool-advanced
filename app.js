6c444f96645bbba5fe3b895411f54867b79218b8
Vladislav Nevzorov <hoverrdev@gmail.com>
2024-04-08 08:37:18 +0300

    Цепочки prototype

--- app.js
+++ app.js
@@ -1,50 +1,12 @@
 'use strict';
 
-/* 
-	Реализовать на функциях и прототипах козрину товаров с методами
-	- Добавить товар
-	- Увеличить число товаров
-	- Уменьшить число товаров (удалить если их 0)
-*/
-
-const product = { id: 1, name: 'Bread', count: 1 };
-
-const Cart = function () {
-	this.products = [];
+const Book = function (title, author) {
+	this.author = author;
+	this.title = title;
 };
+Book.prototype.isRead = false;
 
-Cart.prototype.addProduct = function (product) {
-	if (this.products.find(product => product.id === product.id)) {
-		return;
-	}
-	this.products.push(product);
-};
+const lordOfTheRings = new Book('1', '1');
 
-Cart.prototype.increaseAmount = function (id) {
-	this.products = this.products.map(product => {
-		if (product.id === id) {
-			product.count++;
-			return product;
-		}
-		return product;
-	});
-};
-
-Cart.prototype.decreaseAmount = function (id) {
-	this.products = this.products
-		.map(product => {
-			if (product.id === id) {
-				product.count--;
-				return product;
-			}
-			return product;
-		})
-		.filter(product => product.count > 0);
-};
-
-const good = new Cart();
-good.addProduct(product);
-good.increaseAmount(1);
-good.decreaseAmount(1);
-good.decreaseAmount(1);
-console.log(good);
+console.log(lordOfTheRings.hasOwnProperty('title'));
+console.log(Book.prototype.__proto__);
