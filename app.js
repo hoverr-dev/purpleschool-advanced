b904572421d8d97dd885c455b8d574355e31ae0d
Vladislav Nevzorov <hoverrdev@gmail.com>
2024-04-08 08:46:47 +0300

    Классы

--- app.js
+++ app.js
@@ -3,10 +3,27 @@
 const Book = function (title, author) {
 	this.author = author;
 	this.title = title;
+	this.isRead = false;
 };
-Book.prototype.isRead = false;
+Book.prototype.read = function () {
+	this.isRead = true;
+};
 
-const lordOfTheRings = new Book('1', '1');
+class BookClass {
+	isRead = false;
 
-console.log(lordOfTheRings.hasOwnProperty('title'));
-console.log(Book.prototype.__proto__);
+	constructor(title, author) {
+		this.author = author;
+		this.title = title;
+	}
+
+	read() {
+		this.isRead = true;
+	}
+}
+
+const lotr = new BookClass('lotr', 'Tolkien');
+console.log(lotr);
+console.log(lotr instanceof BookClass);
+lotr.read();
+console.log(lotr.__proto__);
