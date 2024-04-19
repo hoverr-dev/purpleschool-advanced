f4d86120b64d9c742584f8e3c1d9f021606a84b5
Vladislav Nevzorov <hoverrdev@gmail.com>
2024-04-19 11:16:03 +0300

    Упражнение - Мой город

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
 'use strict';
 
-async function getProducts() {
-	try {
-		const productsResponse = await fetch(
-			'https://dummyjson.com/products/'
-		);
-		if (!productsResponse.ok) {
-			throw new Error(productsResponse.status);
-		}
-		const { products } = await productsResponse.json();
-		console.log(products);
+/* 
+	Получить геолокацию пользователя через
+	Geolocation.getCurrentPistion() (WEB API)
+	и по координатам определить город, отправив запрос:
+	https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=00&longitude=00
+*/
 
-		const productResponse = await fetch(
-			'https://dummyjson.com/products/' + products[0].id
+function getGeolocation() {
+	return new Promise((resolve, reject) => {
+		navigator.geolocation.getCurrentPosition(
+			({ coords }) => {
+				resolve({
+					latitude: coords.latitude,
+					longitude: coords.longitude,
+				});
+			},
+			error => {
+				reject(error);
+			}
 		);
-		const product = await productResponse.json();
-		console.log(product);
-	} catch (error) {
-		console.error(error);
-	} finally {
-		console.log('Finally');
-	}
+	});
 }
 
-getProducts();
-console.log('End');
+async function getMyCity() {
+	try {
+		const { latitude, longitude } = await getGeolocation();
+		const response = await fetch(
+			`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`
+		);
+		if (!response.ok) {
+			throw new Error(response.status);
+		}
+		const data = await response.json();
+		console.log(data.locality);
+	} catch (error) {
+		console.error(error);
+	}
+}
+getMyCity();
