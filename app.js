8764e77bf19a71765cda4958fdfa48993db74356
Vladislav Nevzorov <hoverrdev@gmail.com>
2024-04-19 09:45:23 +0300

    Пример работы Event Loop

--- app.js
+++ app.js
@@ -1,33 +1,16 @@
 'use strict';
 
-/* 
-	Сделать функцию, которая принимает строку и текст ошибки и 
-	возвращает уже Promise с JSON из тела запроса
-*/
+console.log(1);
 
-function getData(url, errorMessage, method = 'GET') {
-	return fetch(url, {
-		method,
-	}).then(response => {
-		if (!response.ok) {
-			throw new Error(`${errorMessage} ${response.status}`);
-		}
-		return response.json();
-	});
-}
+setTimeout(() => {
+	console.log(2);
+}, 0);
 
-getData('https://dummyjson.com/products', 'Can not get products')
-	.then(({ products }) => {
-		console.log(products);
-		return getData(
-			'https://dummyjson.com/products/' + products[0].id,
-			'Can not get product'
-		);
-	})
-	.then(data => {
-		console.log(data);
-	})
-	.catch(error => {
-		const el = document.querySelector('.filter');
-		el.innerHTML = error.message;
-	});
+Promise.resolve(3).then(res => {
+	console.log(res);
+	for (let i = 0; i < 1000000000; i++) {}
+});
+
+console.log(4);
+
+for (let i = 0; i < 1000000000; i++) {}
