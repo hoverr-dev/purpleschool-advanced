b35d30025d78f2109486343b15fefbcdd418b947
Vladislav Nevzorov <hoverrdev@gmail.com>
2024-04-20 10:41:42 +0300

    Асинхронные методы

--- app.js
+++ app.js
@@ -1,41 +1,17 @@
 'use strict';
 
-/* 
-	Получить геолокацию пользователя через
-	Geolocation.getCurrentPistion() (WEB API)
-	и по координатам определить город, отправив запрос:
-	https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=00&longitude=00
-*/
-
-function getGeolocation() {
-	return new Promise((resolve, reject) => {
-		navigator.geolocation.getCurrentPosition(
-			({ coords }) => {
-				resolve({
-					latitude: coords.latitude,
-					longitude: coords.longitude,
-				});
-			},
-			error => {
-				reject(error);
-			}
-		);
-	});
-}
-
-async function getMyCity() {
-	try {
-		const { latitude, longitude } = await getGeolocation();
-		const response = await fetch(
-			`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`
-		);
-		if (!response.ok) {
-			throw new Error(response.status);
-		}
-		const data = await response.json();
-		console.log(data.locality);
-	} catch (error) {
-		console.error(error);
+class ProductRepository {
+	async getProducts() {
+		const response = await fetch('https://dummyjson.com/products');
+		console.log(await response.json());
 	}
 }
-getMyCity();
+
+const repo = new ProductRepository();
+repo.getProducts();
+
+const asyncArrow = async () => {
+	const response = await fetch('https://dummyjson.com/products');
+	console.log(await response.json());
+};
+asyncArrow();
