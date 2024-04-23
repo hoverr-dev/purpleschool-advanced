b127be1f2ca9d93f7cd5cbddb7c4b9203c441792
Vladislav Nevzorov <hoverrdev@gmail.com>
2024-04-23 15:14:11 +0300

    Пример всплытия событий

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 'use strict';
 
 const button = document.querySelector('.button');
+const inner = document.querySelector('.inner');
+const wrapper = document.querySelector('.wrapper');
 
-const eventHandler = function (event) {
-	console.log('Event 1');
-};
+button.addEventListener('click', function (event) {
+	console.log('button');
+	console.log(event.target);
+	console.log(event.currentTarget);
+	this.style.backgroundColor = 'purple';
+});
 
-button.addEventListener('mouseover', eventHandler);
+inner.addEventListener('click', function (event) {
+	console.log('inner');
+	console.log(event.target);
+	console.log(event.currentTarget);
+	this.style.backgroundColor = 'blue';
+	// event.stopPropagation();
+});
 
-button.addEventListener('click', event => {
-	console.log('Event 2');
-	button.removeEventListener('click', eventHandler);
+wrapper.addEventListener(
+	'click',
+	function (event) {
+		console.log('wrapper');
+		console.log(event.target);
+		console.log(event.currentTarget);
+		this.style.backgroundColor = 'green';
+	},
+	true
+);
+
+wrapper.addEventListener('click', function (event) {
+	console.log('wrapper');
+	console.log(event.target);
+	console.log(event.currentTarget);
+	this.style.backgroundColor = 'green';
 });
