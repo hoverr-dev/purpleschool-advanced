68ff325e4fa1d238af12177edc8d3d1b80719f55
Vladislav Nevzorov <hoverrdev@gmail.com>
2024-04-30 09:26:21 +0300

    Поиск книг

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -18,11 +18,33 @@ export class MainView extends AbstractView {
 			this.appState,
 			this.appStateHook.bind(this)
 		);
+		this.state = onChange(this.state, this.stateHook.bind(this));
 		this.setTitle('Поиск книг');
 	}
 
 	appStateHook(path) {
-		console.log(path);
+		if (path === 'favorites') {
+			console.log(path);
+		}
+	}
+
+	async stateHook(path) {
+		if (path === 'searchQuery') {
+			this.state.loading = true;
+			const data = await this.loadList(
+				this.state.searchQuery,
+				this.state.offset
+			);
+			this.state.loading = false;
+			this.state.list = data.docs;
+		}
+	}
+
+	async loadList(q, offset) {
+		const res = await fetch(
+			`https://openlibrary.org/search.json?q=${q}&offset=${offset}`
+		);
+		return res.json();
 	}
 
 	render() {
